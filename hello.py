76b0f97c969621a8f2e0288a4efd1c66cb46d9ee
Igor Ramos <i.ramos@aluno.ifsp.edu.br>
2024-08-15 20:24:59 -0300

    e-mail v2

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -8,6 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import mailersend
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,18 +22,9 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-MAILGUN_API_KEY = '8aa4e747c63d7997bc318fdb53b12f74-911539ec-60b1f7c1'
-MAILGUN_DOMAIN = 'sandboxa96fd10e6ec34ba29602647ab40ce9c4.mailgun.org'
-MAILGUN_BASE_URL = 'https://api.mailgun.net/v3/{}'.format(MAILGUN_DOMAIN)
-
-def send_email(subject, text, to):
-    return requests.post(
-        '{}/messages'.format(MAILGUN_BASE_URL),
-        auth=("api", MAILGUN_API_KEY),
-        data={"from": "Excited User <mailgun@{}>'.format(MAILGUN_DOMAIN)",
-              "to": to,
-              "subject": subject,
-              "text": text})
+# Defina a chave da API diretamente
+api_key = 'mlsn.7e4096aed29f00be1663a3354e70feca8fa77421d7eb6ecb3135f15e243603c8'
+mailer = mailersend.NewApiClient(api_key)
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -86,16 +77,27 @@ def index():
             db.session.commit()
             session['known'] = False
 
-            # Send email
-            send_email(
-                subject="New User Registered",
-                text="A new user has registered: {}".format(form.name.data),
-                to="meuprofissagit@gmail.com"
-            )
-            flash('A new user has been registered and an email notification has been sent.', 'success')
+            # Enviar e-mail
+            subject = "New User Registered"
+            text = f"A new user has registered with the username: {form.name.data}."
+            html = f"<p>A new user has registered with the username: <strong>{form.name.data}</strong>.</p>"
+            my_mail = "info@domain.com"
+            recipient = "i.ramos@ifsp.edu.br"
+            
+            try:
+                mailer.send(my_mail, [recipient], subject, html, text)
+                flash('Registration successful, and notification email sent.', 'success')
+            except Exception as e:
+                flash('Registration successful, but there was an error sending the notification email.', 'warning')
+                print(f"Error sending email: {e}")
+
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
